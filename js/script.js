c490822862fbfe546d5b00344d973434aa5202e6
devalex-tsi <83531125+devalex-tsi@users.noreply.github.com>
2025-05-08 21:50:56 +0000

    Deploying to master from @ devalex-tsi/ghostgarage@c45ff8f3d329fe31ba6b760f5bee1d05f1c2d653 🚀

--- js/script.js
+++ js/script.js
@@ -24,3 +24,36 @@ if (mainNav) {
 } else {
     console.error("No .main-nav on the page");
 }
+
+
+//------------------------------------------
+
+
+const navLinks = document.querySelectorAll('.main-nav__item a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href');
+
+        if (targetId !== '#' && targetId.startsWith('#')) {
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                const mainNavHeight = mainNav ? mainNav.offsetHeight : 0;
+
+                const scrollToPosition = targetElement.offsetTop - mainNavHeight;
+                window.scrollTo({
+                    top: scrollToPosition,
+                    behavior: 'smooth'
+                });
+            }
+        } else if (targetId === '#') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
